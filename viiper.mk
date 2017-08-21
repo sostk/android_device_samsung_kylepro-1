d4445ce99760c8bb0a238acc120619816fba61fe
Real man born on semtember <hieunam03@gmail.com>
2017-08-21 16:03:54 +0700

    Update and rename hexagon.mk to viiper.mk

--- viiper.mk
+++ viiper.mk
@@ -1,15 +1,15 @@
-# Inherit some common HEXAGON stuff.
-$(call inherit-product, vendor/hexagon/configs/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := hexagon_kylepro
+PRODUCT_NAME := viper_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
